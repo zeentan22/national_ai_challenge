09fc0c6538c589235c220498033c542af61a0ae5
zeentan22 <e0725293@u.nus.edu>
2023-02-05 15:06:39 +0800

    transfer to model file

---
+++ model/output.py
@@ -0,0 +1,46 @@
+from model import nlp_model
+
+
+
+inputs = [
+    {  # Single word aspect
+        "aspects": ["school"],
+        "sentence": "The school is a conducive environment for me  ."
+    },
+    {
+        "aspects": ["students"],
+        "sentence": "My students are very naughty .",
+        
+    },
+    {
+        "aspects": ["students"],
+        "sentence": "My students are very good .",
+        
+    },
+    {
+        "aspects": ["assignments"],
+        "sentence": "My students are very good .",
+        
+    },
+    {
+        "aspects": ["meeting"],
+        "sentence": "Parent teacher meeting is conducted annually .",
+        
+    },
+    {
+        "aspects" : ["school"],
+        "sentence": "I dislike the school environment ."
+    }
+]
+
+model = nlp_model(inputs)
+model.run()
+# print(repr(model))
+print(model.get_all_aspects_and_labels())
+
+{
+'school': {'count': 2, 'positive': 2, 'netural': 0, 'negative': 0}, 
+'students': {'count': 2, 'positive': 2, 'netural': 0, 'negative': 0}, 
+'meeting': {'count': 1, 'positive': 1, 'netural': 0, 'negative': 0}
+}
+
